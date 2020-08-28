8a5bfa4e0a52230691e1754c4f9523db75902531
Daffa Rabbanee <daffarabbanee@gmail.com>
2020-08-28 10:35:50 +0700

    Initialize project using Create React App

---
+++ src/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hello() {
+    return (
+        <div className="container">
+            <p>Hello React App</p>
+        </div>
+    )
+}
+
+export default Hello;
